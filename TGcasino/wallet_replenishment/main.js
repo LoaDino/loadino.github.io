9a8e33e56905e5f863d653d5e3cef2ec70f04c49
LoaDino <varnacevigor2@gmail.com>
2022-11-04 02:04:52 +0300

    update tg casino #1

--- TGcasino/wallet_replenishment/main.js
+++ TGcasino/wallet_replenishment/main.js
@@ -1,11 +1,14 @@
 
 let tg = window.Telegram.WebApp;
 
+console.log(tg)
+
 window.onload = () => {
     document.querySelector("#submit").onclick = () => {
         let currency = document.querySelector("#choice");
         let sum = document.querySelector("#sum");
 
+        alert("успешно!")
         tg.sendData(sum.value + " " + currency.value);
     }
 }
