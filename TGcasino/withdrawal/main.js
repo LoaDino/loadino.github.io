01365761037f8c4692a5e49028cdd98fb1a63bb6
LoaDino <varnacevigor2@gmail.com>
2022-11-04 02:44:49 +0300

    TG casino #2

---
+++ TGcasino/withdrawal/main.js
@@ -0,0 +1,32 @@
+
+let tg = window.Telegram.WebApp;
+
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
+let money = params.money;
+
+window.onload = () => {
+    let Pmoney = document.getElementById("money");
+
+    if (money != null) {
+        Pmoney.innerHTML = "У вас " + money + " монет";
+    } else {
+        Pmoney.innerHTML = "Ошибка получаения данных. Перезапустите это окно."
+    }
+
+    document.querySelector("#submit").onclick = () => {
+        let sum = document.querySelector("#sum");
+
+        if (money == null) {
+            alert("ошибка данных, перезапустите это окно.")
+        } else {
+            if (Number(money) >= Number(sum.value)) {
+                alert("успешно!");
+                tg.sendData(sum.value + " " + currency.value);
+            } else {
+                alert("невозможно, у вас не хватает монет.");
+            }
+        }
+    }
+}
