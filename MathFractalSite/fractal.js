4ae36b39c676fa7045bb8744394fbfeaf41c3735
LoaDino <74372772+LoaDino@users.noreply.github.com>
2024-05-01 23:54:22 +0300

    version 1.0

--- MathFractalSite/fractal.js
+++ MathFractalSite/fractal.js
@@ -33,7 +33,7 @@ function drawLevyCurve(x, y, length, angle, depth) {
 
 
 function animateLevyCurve() {
-    var length = Math.min(window.innerHeight, window.innerWidth)/2
+    let length = Math.min(window.innerHeight, window.innerWidth)/2
 
 	ctx.clearRect(0, 0, canvas.width, canvas.height)
 	drawLevyCurve(canvas.width/2,canvas.height/4, length, Math.PI / 2, depth)
@@ -93,6 +93,20 @@ function getRandomColor() {
 
 
 function start() {
+	let head = document.getElementsByTagName("head")
+	let style = document.createElement("link")
+	style.rel = 'stylesheet'
+
+	if (window.innerHeight > window.innerWidth) {
+		style.href = 'style2.css'
+	} else {
+		style.href = 'style.css'
+	}
+
+	head[0].appendChild(style)
+
+	ON_SCROLL()
+
 	canvas = document.getElementById('Canvas')
 	ctx = canvas.getContext('2d')
 
@@ -109,3 +123,63 @@ function start() {
 }
 
 window.onload = start
+
+function visible(target) {
+  // Все позиции элемента
+  var targetPosition = {
+      top: window.pageYOffset + target.getBoundingClientRect().top,
+      left: window.pageXOffset + target.getBoundingClientRect().left,
+      right: window.pageXOffset + target.getBoundingClientRect().right,
+      bottom: window.pageYOffset + target.getBoundingClientRect().bottom
+    },
+    // Получаем позиции окна
+    windowPosition = {
+      top: window.pageYOffset,
+      left: window.pageXOffset,
+      right: window.pageXOffset + document.documentElement.clientWidth,
+      bottom: window.pageYOffset + document.documentElement.clientHeight
+    };
+
+  if (targetPosition.bottom > windowPosition.top && // Если позиция нижней части элемента больше позиции верхней чайти окна, то элемент виден сверху
+    targetPosition.top < windowPosition.bottom && // Если позиция верхней части элемента меньше позиции нижней чайти окна, то элемент виден снизу
+    targetPosition.right > windowPosition.left && // Если позиция правой стороны элемента больше позиции левой части окна, то элемент виден слева
+    targetPosition.left < windowPosition.right) { // Если позиция левой стороны элемента меньше позиции правой чайти окна, то элемент виден справа
+    return true
+  } else {
+    return false
+  };
+};
+
+function ON_SCROLL() {
+	let images = document.getElementsByTagName("img")
+
+	for (let image of images) {
+		if (visible(image)) {
+			image.classList.add('active')
+		} else {
+			image.classList.remove('active')
+		}
+	}
+
+	let texts = document.getElementsByTagName('p')
+
+	for (let text of texts) {
+		if (visible(text)) {
+			text.classList.add('active_alt')
+		} else {
+			text.classList.remove('active_alt')
+		}
+	}
+
+	let heads = document.getElementsByTagName('h2')
+
+	for (let head of heads) {
+		if (visible(head)) {
+			head.classList.add('active_alt')
+		} else {
+			head.classList.remove('active_alt')
+		}
+	}
+}
+
+window.onscroll = ON_SCROLL
